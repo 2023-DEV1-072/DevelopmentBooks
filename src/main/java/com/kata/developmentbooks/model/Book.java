4523436f449761992e1b0a553d87faa62be2dadc
2023-DEV1-072 <pavan.tulasi@expleogroup.com>
2023-05-10 15:55:30 +0200

    added spring book starter project template

---
+++ src/main/java/com/kata/developmentbooks/model/Book.java
@@ -0,0 +1,80 @@
+package com.kata.developmentbooks.model;
+
+public class Book {
+    private Long id;
+    private String title;
+    private String author;
+    private String date;
+    private Double price;
+
+
+    public Book() {
+    }
+
+    public Book(String name, String author, String date,Double price) {
+        this.title = name;
+        this.author = author;
+        this.date = date;
+        this.price=price;
+    }
+
+    public Book(Long id, String name, String author, String date,Double price) {
+        this.id = id;
+        this.title = name;
+        this.author = author;
+        this.date = date;
+        this.price=price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "BookEntity{" +
+                "id=" + id +
+                ", name='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", date='" + date + '\'' +
+                ", price='" + price + '\'' +
+                '}';
+    }
+
+}
