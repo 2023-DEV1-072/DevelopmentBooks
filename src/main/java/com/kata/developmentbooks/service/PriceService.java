f0856fba1ccb0d74d2545b47eab3126013019163
2023-DEV1-072 <pavan.tulasi@expleogroup.com>
2023-05-12 13:04:02 +0200

    working copy for getting optimal price from a combination

--- src/main/java/com/kata/developmentbooks/service/PriceService.java
+++ src/main/java/com/kata/developmentbooks/service/PriceService.java
@@ -7,10 +7,8 @@ import com.kata.developmentbooks.utils.BookGroup;
 import org.springframework.stereotype.Service;
 import com.kata.developmentbooks.model.FinalPriceSummary;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class PriceService implements  IPriceService{
@@ -23,13 +21,27 @@ public class PriceService implements  IPriceService{
     public FinalPriceSummary calculatePrice(List<CartOrder> bookList){
 
         HashMap<Long,CartOrder> bookHashMap = BookGroup.spiltBooksToGroup(bookList);
-        List<HashMap<Long,Long>> groups = sortBooksToGroups(bookHashMap);
-
-        FinalPriceSummary finalPriceSummary = new FinalPriceSummary(bookList,getBooksCount(bookList)*Constants.BOOK_PRICE,calculatePriceForCart(groups));
+        List<List<HashSet<Long>>> allPossibleCombinations = getAllPossibleCombinations(bookHashMap);
+        double price = getLeastFinalPriceFromSets(allPossibleCombinations);
+        FinalPriceSummary finalPriceSummary = new FinalPriceSummary(bookList, getTotalBooksCountInCart(bookList)*Constants.BOOK_PRICE,price);
         return finalPriceSummary;
     }
 
-    private long getBooksCount(List<CartOrder> bookList){
+    private double getLeastFinalPriceFromSets(List<List<HashSet<Long>>> allPossibleCombinations){
+
+        List<Double> allPriceForCombinations = allPossibleCombinations.stream().map(eachCombination-> {
+            double priceForOnePossibleCombination =0;
+            for (HashSet<Long> eachBookSet : eachCombination) {
+
+                priceForOnePossibleCombination+=calculateDiscountForGroup(eachBookSet.size(),Constants.BOOK_PRICE);
+            }
+            return priceForOnePossibleCombination;
+        }).collect(Collectors.toList());
+        double min = allPriceForCombinations.stream().min(Comparator.naturalOrder()).get();
+        return min;
+    }
+
+    private long getTotalBooksCountInCart(List<CartOrder> bookList){
         long booksCount =0l;
         for (CartOrder cartOder:bookList
              ) {
@@ -38,18 +50,10 @@ public class PriceService implements  IPriceService{
         return booksCount;
     }
 
-    private double calculatePriceForCart( List<HashMap<Long,Long>> groups){
-       double finalPrice =0;
-        for (HashMap<Long,Long> eachGroup:groups) {
+    private double calculateDiscountForGroup(int bookCountInGroup,double bookPrice) {
 
-                finalPrice+=calculateDiscountForGroup(eachGroup.size(), Constants.BOOK_PRICE);
-        }
-        return finalPrice;
-    }
-    private double calculateDiscountForGroup(int bookCount,double bookPrice) {
-
-        double actualCost = bookCount * bookPrice;
-        switch(bookCount){
+        double actualCost = bookCountInGroup * bookPrice;
+        switch(bookCountInGroup){
             case 1: return bookPrice;
             case 2: return actualCost * (1 - (5.0 / 100));
             case 3: return actualCost * (1 - (10.0 / 100));
@@ -61,42 +65,70 @@ public class PriceService implements  IPriceService{
 
     }
 
-    private List<HashMap<Long,Long>> sortBooksToGroups(HashMap<Long,CartOrder> bookHashMap){
-        List<HashMap<Long,Long>> bookGroups =  initiateListofGroups(getLargestBookOrderCount(bookHashMap));
-        for (Map.Entry<Long, CartOrder> map : bookHashMap.entrySet()) {
-            Book book =  map.getValue().getBook();
-            long quantity = map.getValue().getQuantity();
-            for(int i=0;i<quantity;i++){
-                bookGroups = addBookToGroup(bookGroups,book);
-            }
-        }
-        return bookGroups;
-    }
     private List<HashMap<Long,Long>> addBookToGroup(List<HashMap<Long,Long>> bookGroup,Book book){
         for (HashMap<Long,Long> bookMap: bookGroup) {
-            if(bookMap.get(book.getId())==null){
+            if(!bookMap.containsKey(book.getId())){
                 bookMap.put(book.getId(),1l);
                 break;
             }
         }
         return bookGroup;
     }
-    private List<HashMap<Long,Long>> initiateListofGroups(long size){
-        List<HashMap<Long,Long>> bookGroups =  new ArrayList<>();
-        for(int i=0;i<size;i++){
-            bookGroups.add(new HashMap<Long,Long>());
-        }
-        return bookGroups;
-    }
-    private long getLargestBookOrderCount(HashMap<Long,CartOrder> bookHashMap){
-        long largestVoulme=0;
 
-        for (Map.Entry<Long, CartOrder> map : bookHashMap.entrySet()) {
-            long quantity = map.getValue().getQuantity();
-            if(largestVoulme<quantity){
-                largestVoulme = quantity;
+
+    private  List<List<HashSet<Long>>> getAllPossibleCombinations(HashMap<Long,CartOrder> allBooksInCart){
+        List<List<HashSet<Long>>> allCombinations = new ArrayList<>();
+        for(long i=Constants.BOOK_COUNT;i>0;i--){
+            allCombinations.add(formSetsForGivenLimit(flattenHashMapToListOfBookIds(allBooksInCart),i));
+        }
+    return allCombinations;
+    }
+
+    private  List<HashSet<Long>> formSetsForGivenLimit(List<Long> allBooksInCart,long limit){
+        List<HashSet<Long>> toReturn = new ArrayList<>();
+       for(Long bookId: allBooksInCart){
+
+
+           if(doesListHasElement(toReturn,bookId,limit) ){
+               HashSet<Long> newBookSet = new HashSet<Long>();
+               newBookSet.add(bookId);
+               toReturn.add(newBookSet);
+
+           }else{
+               for(HashSet<Long> set:toReturn){
+                   if(!set.contains(bookId)&&set.size()<limit){
+                       set.add(bookId);
+                       break;
+                   }
+               }
+           }
+       }
+       return toReturn;
+    }
+    /*
+       for a given bookid and list of book sets
+            check if all the book sets in list has the limit reached
+            (or)
+            it already has the element
+     */
+    private boolean doesListHasElement(List<HashSet<Long>> list,long bookId,long limit){
+        boolean hit = true;
+        for(HashSet<Long> set:list){
+            if(!set.contains(bookId)&&set.size()<limit){
+                hit = false;
             }
         }
-        return largestVoulme;
+        return hit;
     }
+    private List<Long> flattenHashMapToListOfBookIds( HashMap<Long,CartOrder> allBooksInCart){
+        List<Long> allBooks = new ArrayList<Long>();
+        for (Map.Entry<Long, CartOrder> set :
+                allBooksInCart.entrySet()) {
+            set.getValue();
+            for(int i=0;i<set.getValue().getQuantity();i++)
+                allBooks.add(set.getKey());
+        }
+        return allBooks;
+    }
+
 }
