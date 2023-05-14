950d7636e7392481a9659aa196dd2b2031cf4c9b
2023-DEV1-072 <pavan.tulasi@expleogroup.com>
2023-05-14 22:25:24 +0200

    added more test cases to controller

--- src/test/java/com/kata/developmentbooks/controller/BookControllerTest.java
+++ src/test/java/com/kata/developmentbooks/controller/BookControllerTest.java
@@ -1,5 +1,7 @@
 package com.kata.developmentbooks.controller;
 
+import com.google.gson.Gson;
+import com.kata.developmentbooks.utils.TestData;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +12,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 @ExtendWith(SpringExtension.class)
@@ -25,5 +28,20 @@ public class BookControllerTest {
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(jsonPath("$").isArray())
                 .andExpect(jsonPath("$",hasSize(5)));
+    }
+    @Test
+    @DisplayName("validate price of two books in a set is 95")
+    void getPriceForTwoUniqueBooksTest() throws Exception {
+        String json = ObjectToJson(TestData.getTwoUniqueBooks());
+        mockMvc.perform(post("/api/books/calculateBookPrice")
+                .contentType("application/json")
+                .content(json))
+                .andExpect(jsonPath("$.finalPrice").value(95.0))
+                .andExpect(status().isOk());
+    }
+    private String ObjectToJson(Object toConvert){
+        Gson gson = new Gson();
+        String json = gson.toJson(toConvert);
+        return json;
     }
 }
